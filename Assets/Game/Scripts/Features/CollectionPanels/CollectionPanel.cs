ea9ad8a5a22f588d6dd99096a0dbc6eaae2fb1c3
Vladimir Sinkovets <vovasin49@gmail.com>
2025-05-13 14:33:02 +0300

    Add panel

---
+++ Assets/Game/Scripts/Features/CollectionPanels/CollectionPanel.cs
@@ -0,0 +1,26 @@
+﻿using Assets.Game.Scripts.Features.Spawner;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Game.Scripts.Features.CollectionPanels
+{
+    public class CollectionPanel : MonoBehaviour, ICollectionPanel
+    {
+        [SerializeField] private RectTransform _panel;
+
+        private List<RectTransform> _uiFigures;
+
+        private void Start()
+        {
+            foreach (RectTransform child in _panel)
+            {
+                _uiFigures.Add(child);
+            }
+        }
+
+        public void Take(Figure figure)
+        {
+            
+        }
+    }
+}
